80428d46b47296c0f2e192c4d2668294a7842041
Dilan Dev <dilan.developer22@gmail.com>
2024-06-08 12:32:56 -0500

    adding js without errors

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const $btn__generate_password = document.getElementById(
-  "btn__generate_password"
+	"btn__generate_password"
 );
 
 $btn__generate_password.addEventListener("click", () => {
-  alert(`
+	alert(`
   Your password is: 
   ${generatePassword(
-    lengthPassword,
-    includeLettersUpperCase,
-    includeLettersLowerCase,
-    includeNumber,
-    includeSymbol
-  )}`);
+		lengthPassword,
+		includeLettersUpperCase,
+		includeLettersLowerCase,
+		includeNumber,
+		includeSymbol
+	)}`);
 });
 
 const symbols = "!'#$%&,()*+'-./:;<=>?@[]~^_{|}";
@@ -20,24 +20,24 @@ const lettersLowerCase = "abcdefghijklmnñopqrstuvwxyz";
 const numbers = "0123456789";
 
 function generatePassword(
-  length,
-  includeLettersLowerCase,
-  includeLettersUpperCase,
-  includeNumber,
-  includeSymbol
+	length,
+	includeLettersLowerCase,
+	includeLettersUpperCase,
+	includeNumber,
+	includeSymbol
 ) {
-  let password = "";
-  let allChareters = "";
+	let password = "";
+	let allChareters = "";
 
-  allChareters += includeLettersLowerCase ? lettersLowerCase : "";
-  allChareters += includeLettersUpperCase ? lettersUpperCase : "";
-  allChareters += includeNumber ? numbers : "";
-  allChareters += includeSymbol ? symbols : "";
-  for (let i = 0; i <= length; i++) {
-    let randomIndex = Math.floor(Math.random() * allChareters.length);
-    password += allChareters[randomIndex];
-  }
-  return password;
+	allChareters += includeLettersLowerCase ? lettersLowerCase : "";
+	allChareters += includeLettersUpperCase ? lettersUpperCase : "";
+	allChareters += includeNumber ? numbers : "";
+	allChareters += includeSymbol ? symbols : "";
+	for (let i = 0; i <= length; i++) {
+		let randomIndex = Math.floor(Math.random() * allChareters.length);
+		password += allChareters[randomIndex];
+	}
+	return password;
 }
 
 let lengthPassword = 10;
@@ -47,13 +47,11 @@ let includeNumber = true;
 let includeSymbol = true;
 
 let passwordGenerate = generatePassword(
-  lengthPassword,
-  includeLettersUpperCase,
-  includeLettersLowerCase,
-  includeNumber,
-  includeSymbol
+	lengthPassword,
+	includeLettersUpperCase,
+	includeLettersLowerCase,
+	includeNumber,
+	includeSymbol
 );
 
-alert(`Your new password is: ${passwordGenerate}`);
-
-
+// alert(`Your new password is: ${passwordGenerate}`);
